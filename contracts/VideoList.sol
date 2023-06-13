03ba27ce1c39ad2bf7c7b045be497f81b647c9a0
Sergi Rene <sergi@datahop.network>
2023-06-13 13:14:39 +0200

    init version contracts

---
+++ contracts/VideoList.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract VideoList is ERC721, ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("VideoList", "VDL") {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function createItem(address player, string memory tokenURI) public returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- contracts/VideoMarketplace.sol
+++ contracts/VideoMarketplace.sol
@@ -3,15 +3,31 @@ pragma solidity ^0.8.17;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {VideoMarketPlaceERC20Token} from "./VideoMarketPlaceERC20Token.sol";
+import {VideoList} from "./VideoList.sol";
+import {PullPayment} from "@openzeppelin/contracts/security/PullPayment.sol";
 
-contract VideoMarketPlace is Ownable {
+struct Listing {
+    address seller;
+    uint256 price;
+    string uri;
+}
 
-    /// @notice Address of the token used as payment for the bets
+error CallbackNotAuthorized();
+error ListingDoesNotExist();
+error InsufficentFunds();
+
+contract VideoMarketPlace is Ownable,PullPayment {
+
+    /// @notice Address of the token used as payment for the videos
     VideoMarketPlaceERC20Token public paymentToken;
+    /// @notice Video NFTtoken 
+    VideoList public nftToken;
     /// @notice Amount of tokens given per ETH paid
     uint256 public purchaseRatio;
     /// @notice Amount of tokens required for placing a bet that goes for the owner pool
     uint256 public marketFee;
+
+    mapping(uint256 => Listing) public listings;
 
     /// @notice Constructor function
     /// @param tokenName Name of the token used for payment
@@ -25,8 +41,37 @@ contract VideoMarketPlace is Ownable {
         uint256 _marketFee
     ) {
         paymentToken = new VideoMarketPlaceERC20Token(tokenName, tokenSymbol);
+        nftToken = new VideoList();
         purchaseRatio = _purchaseRatio;
         marketFee = _marketFee;
     }
+
+
+    function createListing(
+        uint256 price,
+        string calldata uri
+    ) external returns (uint256) {
+        uint256 nftId = nftToken.createItem(msg.sender, uri);
+        listings[nftId] = Listing(msg.sender, price, uri);
+        return nftId;
+    }
+
+    /// @notice Pay for a listing
+    /// @dev Buyer pays the price for the listing, which can be withdrawn by the seller later; emits an event
+    /// @return requestId The id of the reencryption request associated with the purchase
+    function buyListing(uint256 nftId) external payable returns (string memory) {
+        Listing memory listing = listings[nftId];
+        if (listing.seller == address(0)) {
+            revert ListingDoesNotExist();
+        }
+        if (msg.value < listing.price) {
+            revert InsufficentFunds();
+        }
+        _asyncTransfer(listing.seller, msg.value);
+        //uint256 requestId = oracle.requestReencryption(cipherId, buyerPublicKey);
+        //emit NewSale(msg.sender, listing.seller, requestId, cipherId);
+        return listing.uri;
+    }
+
 
 }
